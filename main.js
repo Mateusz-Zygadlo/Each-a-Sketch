1a36cf97a463f1b4c17f45e37f0d6e5ca8e7c9f8
Mateusz <mateuszzygadlo00@gmail.com>
2021-08-08 19:03:30 +0200

    Edit

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 const grid = document.querySelector('.grid');
 const score = document.querySelector('.score');
 const input = document.querySelector('.input');
+
+const startPage = document.querySelector('.startPage');
+const buttonStart = document.querySelector('.start');
+
 let count = 1;
 
 let divs;
 let itemId;
 let bgColor;
-let rgbRandom;
-let test;
+let rgbRandom = 'orange';
+let randomColor;
 
-function createDiv(n){
+function createandClassDiv(n){
     for(let i = 0; i < n; i++){
         for(let j = 0; j < n; j++){
             let divOne = document.createElement('div');
-            divOne.classList.add('b');
+            divOne.classList.add('div');
             grid.appendChild(divOne);
         }
     }
     addId();
-    divs = document.getElementsByClassName('b');
+    divs = document.getElementsByClassName('div');
 }
 function addId(){
-    let div = document.querySelectorAll('.b');
+    let div = document.querySelectorAll('.div');
     div.forEach(item => {
         item.dataset.id = count;
         count++;
     })
     count = 1;
 }
-function click(){
+function mouseOver(){
     [...divs].forEach(elem => {
         elem.addEventListener('mouseover', (e) => {
             itemId = e.target;
@@ -40,52 +44,38 @@ function click(){
 function changeColor(){
     bg = itemId;
     if(bgColor == 'rgb'){
-        const red = Math.floor(Math.random() * 255);
-        const green = Math.floor(Math.random() * 255);
-        const blue = Math.floor(Math.random() * 255);
-    
-        let final = `rgb(${red}, ${green}, ${blue})`;
-
-        bg.style.backgroundColor = final;
-    }else if(bgColor == 'clear'){
-        bgColor = 'white';
+        randomRgb();
+        bg.style.backgroundColor = randomColor;
+    }else{
+        bg.style.backgroundColor = bgColor;
     }
-    bg.style.backgroundColor = bgColor;
 }
-
 function randomRgb(){
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
 
+    randomColor = `rgb(${red}, ${green}, ${blue})`;
     return `rgb(${red}, ${green}, ${blue})`;
 }
-function backgroundRgb(){
-    setInterval(() => {
-        bgColor = rgbRandom;
-    }, 1);
-}
-
 function clear(){
     [...divs].forEach(div => {
         div.style.backgroundColor = 'white';
     })
 }
-
 function setButton(){
     let buttons = document.getElementsByClassName('choose');
 
     [...buttons].forEach(btn => {
         btn.addEventListener('click', (e) => {
             bgColor = e.target.value;
-            console.log(bgColor);
             if(bgColor == 'eraser'){
                 bgColor = 'white';
             }else if(bgColor == 'clear'){
-                bgColor = 'white';
+                bgColor = clear();
             }else if(bgColor == 'rgb'){
                 bgColor = 'rgb';
-            }else if(bgColor = 'randomColor'){
+            }else if(bgColor == 'randomColor'){
                 bgColor = rgbRandom;
             }
         })
@@ -98,6 +88,10 @@ input.addEventListener('change', () => {
     grid.classList.remove('t');
     grid.style.gridTemplateColumns = `repeat(${input.value}, 1fr)`;
     score.textContent = input.value;
-    createDiv(input.value);
-    click();
+    createandClassDiv(input.value);
+    mouseOver();
+})
+
+buttonStart.addEventListener('click', () => {
+    startPage.classList.toggle('popup');
 })
